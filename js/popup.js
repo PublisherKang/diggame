7cacce84aa849fe8d19a36675a8522568c3636e8
vstro kbs <kbs21c1987@gmail.com>
2022-01-21 16:00:12 +0900

    add

--- js/popup.js
+++ js/popup.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
+    // button false
     // 인벤토리 팝업
-    const invenPopBtn = $(".inventory_btn")
-    const backBtn = $(".back_btn")
+    const invenPopBtn = $(".inventory_btn");
+    const backBtn = $(".back_btn");
+
     invenPopBtn.click(function(ev){
         ev.preventDefault();
         $(".inven_popup").show();
     });
 
     // Back 버튼
-    backBtn.click(function(){
+    backBtn.click(function(ev){
+        ev.preventDefault();
         $(this).parent().parent().hide();
     });
 
