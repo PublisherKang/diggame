d2829440eba5ee9e8314816dca887de356f9e993
vstro kbs <kbs21c1987@gmail.com>
2022-03-18 15:53:20 +0900

    add

--- js/digtocPage.js
+++ js/digtocPage.js
@@ -26,3 +26,66 @@
   });
   
 }());
+
+
+// 모바일 메뉴 열기
+(function(){
+  const mobileGnbBtn = document.querySelector(".menu_btn");
+  const mobileSlideMenu = document.querySelector(".mobi_side");
+  const mobileSlideMenuInner = document.querySelector(".side_inner");
+  const closeSide = document.querySelector(".mobi_side .overlay");
+  console.log(closeSide);
+
+  mobileGnbBtn.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    
+    mobileSlideMenu.style.visibility = "visible";
+    mobileSlideMenuInner.style.transform = "translateX(0)";
+
+    document.querySelector("body").classList.add("stop-scroll");
+  });
+
+  closeSide.addEventListener("click", (ev) => {
+    mobileSlideMenu.style.visibility = "hidden";
+    mobileSlideMenuInner.style.transform = "translateX(-100%)";
+    document.querySelector("body").classList.remove("stop-scroll");
+  });
+
+}());
+
+
+// gameStartView();
+// function gameStartView(){
+//   const startBtn = document.querySelector(".start_btn");
+//   // let sectionScroll = contSection.getBoundingClientRect().top;
+  
+//   // console.log(sectionScroll);
+  
+//   const contSection = document.querySelector(".cont2");
+//   console.log(contSection.scrollTop);
+  
+//   document.addEventListener('scroll', function(ev){
+//     // const windowTop = this.scrollTop;
+//     // console.log(this.clientTop);
+//     console.log(this.offsetTop);
+//     // if(sectionScroll < 0){
+//     //   startBtn.style.transform = "translateX(0)";
+//     // }
+//   });
+  
+// }
+
+$(window).scroll(function(){
+  const thisScrT = $(this).scrollTop();
+  const section2 = $(".cont2").offset().top;
+  const startBtn = $(".start_btn");
+  console.log(thisScrT);
+  console.log(section2);
+  if(thisScrT > section2){
+    startBtn.css("transform", "translateX(0)");
+  }else{
+    startBtn.css({
+      "transform": "translateX(100%)",
+    });
+  }
+});
